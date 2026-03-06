f3ed740745a7a9c532e5ba041a61a9baf3227323
psymon-ai <psymon.dev@gmail.com>
2026-03-06 11:57:49 +0900

    Initial commit: Add AstroPaper blog with TOC sidebar and Giscus comments

---
+++ README.md
@@ -0,0 +1,29 @@
+---
+layout: ../layouts/AboutLayout.astro
+title: "About Me"
+---
+
+# psymon's blog
+
+![Astro Paper](public/About-me.png)
+
+안녕하세요. 박성열입니다. 2018년 우연한 계기로 소프트웨어 개발을 접한 뒤 많은 경험을 했습니다. 임베디드 소프트웨어 개발로 시작해 MFC, 영상처리, 웹, 앱, LLM, 그리고 창업과 폐업까지 겪고 나니 이제 한 번쯤 그간의 경험을 정리할 필요성을 느껴 블로그를 개설했습니다.
+
+## Experience
+
+이력 사항이라기보단 그간 거쳐온 분야들이자 이 블로그의 글감들입니다.
+
+- Startup
+- LLM Fine-tuning
+- Image processing
+- Frontend Development
+- Cross-platform development
+- Linux kernel development
+
+and so much more.
+
+## Contact Me
+
+이외 궁금한 사항이나 협업 문의는 ✉️[psymon.dev@gmail.com](mailto:psymon.dev@gmail.com)으로 연락바랍니다.
+
+감사합니다.👋
---
+++ src/config.ts
@@ -0,0 +1,27 @@
+export const SITE = {
+  website: "https://github.com/psymon-ai/psymon.github.io.git", // replace this with your deployed domain
+  author: "psymon",
+  profile: "https://github.com/psymon-ai/psymon.github.io.git",
+  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
+  title: "psymon.dev",
+  ogImage: "../public/About.png", // replace this with your og image path
+  lightAndDarkMode: true,
+  postPerIndex: 8,
+  postPerPage: 8,
+  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+  showArchives: true,
+  showBackButton: true, // show back button in post detail
+  editPost: {
+    enabled: false,
+    text: "Edit page",
+    url: "",
+  },
+  dynamicOgImage: true,
+  dir: "ltr", // "rtl" | "auto"
+  lang: "ko", // html lang code. Set this empty and default will be "en"
+  timezone: "Asia/Seoul", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  comments: {
+    enabled: true,
+    provider: "giscus", // currently only giscus is supported
+  },
+} as const;
